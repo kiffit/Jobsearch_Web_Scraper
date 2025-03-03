9d2704822cb4e5379a25e9e43106a93310dfc0f4
Jerry Cai <jerrycai92@gmail.com>
2025-03-02 22:04:15 -0600

    Updated code with Evan's validation + my previous.

--- Develop Phase/Code/main.py
+++ Develop Phase/Code/main.py
@@ -1,3 +1,4 @@
+import time
 from bs4 import BeautifulSoup
 from seleniumbase import Driver
 
@@ -17,94 +18,71 @@ def main():
     jobs_list = []
     soup = "ERR"
     data_frame = []
+    index  = -1
 
     # Grab user search parameters
-    user_keywords, user_location = general_check(get_user_input(), "Unable to grab search parameters...")
+    user_keywords, user_location = get_user_input()
 
     # Google
     base_url_google = "https://www.google.com/search"
-    search_url_google = general_check(create_user_search_parameters(user_keywords, user_location, base_url_google, query), "Unable to generate search...")
-    soup = general_check(get_html_code(search_url_google), "Unable to load soup...")
-    general_check(find_job_data(soup, jobs_list), "Unable to initialize job search...")
-
-# Make a function that validates strings
-def input_valid_str(input_check):
-    check = False
-    char_check = ''
-    for char in input_check:
-        try:
-            char_check = int(char)
-            check = True
-            break
-        except:
-            pass
-    if check:
-        return False
-    else:
-        return True
-
-# Make a function that checks if a statement executes properly, throws specified error statement otherwise
-def general_check(statement, err_statement):
-    try:
-        statement
-        return statement
-    except:
-        print(err_statement)
-
+    search_url_google = create_user_search_parameters(user_keywords, user_location, base_url_google, query)
+    soup = get_html_code(search_url_google)
+    jobs_list = jobs_list_create_helper(soup, 'tNxQIb PUpOsf')
+    find_job_data(soup, jobs_list, 'tNxQIb PUpOsf', 0)
+    find_job_data(soup, jobs_list, 'wHYlTd MKCbgd a3jPc', 1)
+    find_job_data(soup, jobs_list, 'wHYlTd FqK3wc MKCbgd', 2)
+    find_job_data(soup, jobs_list, 'Yf9oye', 3)
+    find_job_data(soup, jobs_list, 'nNzjpf-cS4Vcb-PvZLI-Ueh9jd-LgbsSe-Jyewjb-tlSJBe', 4)
+    print(jobs_list)
 
 def get_user_input():
     print("Please enter any keyword that you would like with the spaces being replaced by +")
     print("\tExample: Data+Scientist, Computer+Engineer, etc..")
     user_keywords = input("\tEnter: ")
 
-    while True:
-        check = input_valid_str(user_keywords)
-        if check:
-            break
-        else:
-            user_keywords = input("\tInvalid Input...\n\tEnter: ")
-
     print("\nPlease enter any location that you would like with the spaces being replaced by +")
     print("\tExample: Kearney+Nebraska, New+York, etc...")
     user_location = input("\tEnter: ")
 
-    while True:
-        check = input_valid_str(user_location)
-        if check:
-            break
-        else:
-            user_location = input("\tInvalid Input...\n\tEnter: ")
-
     return user_keywords, user_location
+
+
+def input_valid(user_keywords, user_location):
+    return
 
 
 def create_user_search_parameters(user_keywords, user_location, base_url_google, query):
     query = f"?q={user_keywords}+jobs+in+{user_location}&ibp=htl;jobs"
     search_url = base_url_google + query
-
     print("Search URL:", search_url)
     return search_url
 
 
 def get_html_code(search_url):
-    driver = general_check(Driver(browser="Chrome", headless=False), "Unable to load driver...")
-    general_check(driver.get(search_url), "Unable to load webpage...")
+    driver = Driver(browser="Chrome", headless=False)
+    driver.get(search_url)
+    bottom_height = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(.5)
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == bottom_height:
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            return soup
+        bottom_height = new_height
 
-    soup = general_check(BeautifulSoup(driver.page_source, 'html.parser'), "Unable to parse webpage...")
-    return soup
-
-
-def find_job_data(soup, jobs_list):
-    job_cards = soup.find_all('div', class_='tNxQIb PUpOsf')
-    counter = 0
-
+def jobs_list_create_helper(soup, class_name):
+    job_cards = soup.find_all('div', class_=f'{class_name}')
     rows, cols = (len(job_cards), 5)
     jobs_list = [[0 for i in range(cols)] for j in range(rows)]
+    return jobs_list
+
+def find_job_data(soup, jobs_list, class_name, index):
+    job_cards = soup.find_all('div', class_=f'{class_name}')
+    counter = 0
     for equipment_type in job_cards:
-        jobs_list[counter][0] = equipment_type.text
+        jobs_list[counter][index] = equipment_type.text
         counter += 1
-    print(len(job_cards))
-    print(jobs_list)
     return jobs_list
 
 
